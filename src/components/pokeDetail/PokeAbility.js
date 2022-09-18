404b67e5417b89380b94ac8f373288b5ea99bf3b
artebleker <artebleker@gmail.com>
2022-09-18 18:47:12 -0300

    fix: ability detail info

--- src/components/pokeDetail/PokeAbility.js
+++ src/components/pokeDetail/PokeAbility.js
@@ -1,28 +1,33 @@
-import React , {useState, memo, useEffect} from 'react'
-import axios from 'axios'
-const PokeAbility = ({ability}) => {
-const [isModalAbility, setIsModalAbility] = useState(false)
-const [abilityData, setAbilityData] = useState([])
+import React, { useState, memo, useEffect } from "react";
+import axios from "axios";
+const PokeAbility = ({ ability }) => {
+  const [isModalAbility, setIsModalAbility] = useState(false);
+  const [abilityData, setAbilityData] = useState([]);
 
-useEffect(() => {
+  useEffect(() => {
     const endPointPokemon = `https://pokeapi.co/api/v2/ability/${ability}`;
     axios
       .get(endPointPokemon)
       .then((data) => {
         setAbilityData(data);
-
       })
       .catch((err) => console.error(err));
-    }, [ability]);
-// console.log(abilityData.data.effect_entries[0].effect)
+  }, [ability]);
+  console.log(abilityData.data);
   return (
     <div>
-<button onClick={()=> setIsModalAbility(!isModalAbility)}>{ability}</button>
-<div className={isModalAbility? 'abilityOn' : 'abilityOff'}>
-{/* <p>{abilityData.data.effect_entries[1].effect}</p> */}
-</div>
+      <button onClick={() => setIsModalAbility(!isModalAbility)}>
+        {ability}
+      </button>
+      <div className={isModalAbility ? "abilityOn" : "abilityOff"}>
+        {abilityData.data ? (
+          <p>{abilityData.data.effect_entries[1].effect}</p>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default memo(PokeAbility)
+export default memo(PokeAbility);
