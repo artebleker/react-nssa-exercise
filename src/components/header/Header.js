f81d8336bb05678d4d4c8a1f50e00b34641bd145
artebleker <artebleker@gmail.com>
2022-09-20 09:44:18 -0300

    fix: header style

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,8 +3,8 @@ import './header.css'
 import pokedex from '../../img/pokedex.png'
 const Header = () => {
   return (
-    <div>
-        <img src={pokedex} alt='pokedex' width={"200px"}/>
+    <div className='header'>
+       <a href='https://www.linkedin.com/in/julian-bleker/' target='blank'> <img src={pokedex} alt='pokedex' width={"200px"}/></a>
     </div>
   )
 }
